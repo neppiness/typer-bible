f1de4d5d4cbd0cfc81903b28fcf65afdddb3e8e3
neppiness <0414kjh@naver.com>
2023-05-06 19:22:53 +0900

    Initial commit

---
+++ src/main/java/typer/bible/BibleApplication.java
@@ -0,0 +1,14 @@
+package typer.bible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/typer/bible/BibleApplicationTests.java
@@ -0,0 +1,13 @@
+package typer.bible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
