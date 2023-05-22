3e6c4f9d7bdab37b6b3e9bfb7395032051c97ca7
neppiness <0414kjh@naver.com>
2023-05-23 00:01:48 +0900

    test: minor refactoring for BookOrderTest (src/test/java/typer/bible/domain)

--- src/test/java/typer/bible/domain/BookOrderTest.java
+++ src/test/java/typer/bible/domain/BookOrderTest.java
@@ -3,24 +3,20 @@ package typer.bible.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-
 class BookOrderTest {
 
     final static int lastIndex = BookName.values().length - 1;
-    final static HashMap<BookName, BookName> prevOrder = BookOrder.getPrevOrder();
-    final static HashMap<BookName, BookName> nextOrder = BookOrder.getNextOrder();
 
     @Test
     void getPrevOrder() {
         for (int i = 1; i <= lastIndex; i++) {
             BookName bookName = BookName.values()[i];
             BookName prevBookName = BookName.values()[i - 1];
-            Assertions.assertThat(prevBookName).isEqualTo(prevOrder.get(bookName));
+            Assertions.assertThat(prevBookName).isEqualTo(BookOrder.getPrevBookName(bookName));
             System.out.print(prevBookName.toString() + "은 ");
             System.out.println(bookName.toString() + "의 이전 경입니다.");
         }
-        Assertions.assertThat(BookName.REVELATION).isEqualTo(prevOrder.get(BookName.GENESIS));
+        Assertions.assertThat(BookName.REVELATION).isEqualTo(BookOrder.getPrevBookName(BookName.GENESIS));
         System.out.print(BookName.REVELATION.toString() + "은 ");
         System.out.println(BookName.GENESIS.toString() + "의 이전 경입니다.");
     }
@@ -30,11 +26,11 @@ class BookOrderTest {
         for (int i = 0; i < lastIndex; i++) {
             BookName bookName = BookName.values()[i];
             BookName nextBookName = BookName.values()[i + 1];
-            Assertions.assertThat(nextBookName).isEqualTo(nextOrder.get(bookName));
+            Assertions.assertThat(nextBookName).isEqualTo(BookOrder.getNextBookName(bookName));
             System.out.print(nextBookName.toString() + "은 ");
             System.out.println(bookName.toString() + "의 다음 경입니다.");
         }
-        Assertions.assertThat(BookName.GENESIS).isEqualTo(nextOrder.get(BookName.REVELATION));
+        Assertions.assertThat(BookName.GENESIS).isEqualTo(BookOrder.getNextBookName(BookName.REVELATION));
         System.out.print(BookName.GENESIS.toString() + "은 ");
         System.out.println(BookName.REVELATION.toString() + "의 다음 경입니다.");
     }
