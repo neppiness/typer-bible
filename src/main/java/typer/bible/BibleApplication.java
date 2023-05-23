cb8e0bc96a90efd460c42d5d41c176857639d01e
neppiness <0414kjh@naver.com>
2023-05-23 18:26:52 +0900

    update: BibleApplication class minor change
    Using @ComponentScan annotation to scan components

--- src/main/java/typer/bible/BibleApplication.java
+++ src/main/java/typer/bible/BibleApplication.java
@@ -2,14 +2,13 @@ package typer.bible;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
+@ComponentScan
 public class BibleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibleApplication.class, args);
 	}
-
 }
